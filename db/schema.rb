29ba630e03f68097c019f214254a7ba01a34b3bf
James Bunyon <bunyonjames@gmail.com>
2023-03-07 16:11:11 +0100

    transportation form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_162056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_133130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_162056) do
     t.bigint "trip_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "return_day"
+    t.date "return_day"
     t.index ["trip_id"], name: "index_transportations_on_trip_id"
   end
 
