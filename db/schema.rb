c3c30193753752054cd8cc3a579962ec251904e4
Fran Rodrigo <franjrodrigop@gmail.com>
2023-03-07 16:31:19 +0100

    accommodation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_162056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_132600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_162056) do
     t.bigint "trip_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "return_day"
+    t.date "return_day"
     t.index ["trip_id"], name: "index_accommodations_on_trip_id"
   end
 
