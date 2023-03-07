1af6c2b37b9880bd4fa9c023943dc45a9af86e0d
Lena Mueller <lenakristinmueller@gmail.com>
2023-03-07 11:47:53 +0100

    index search

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,13 @@
 class TripsController < ApplicationController
   def index
-    @trips = current_user.trips.all
+    if params[:query].present?
+      @trips = current_user.Trip.search_by_title(params[:query])
+    else
+      @trips = current_user.trips.all
+    end
   end
+
+    # @trips = current_user.trips.all
 
   def new
     @trip = Trip.new
