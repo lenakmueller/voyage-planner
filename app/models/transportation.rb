e07fb231c4af76290b04dd3f9eca06e352d7a3c0
James Bunyon <bunyonjames@gmail.com>
2023-03-08 16:54:43 +0100

    location selector

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -6,6 +6,7 @@ class Transportation < ApplicationRecord
   validates :departure, presence: true
   validates :return_day, presence: true
   validates :transport_mode, presence: true
-
+  validates :location, presence: true
+  
   enum transport_mode: { flight: 0, train: 1, boat: 2, bike: 3, car: 4 }
 end
