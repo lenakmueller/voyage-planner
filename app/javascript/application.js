4c8af246bc3ca37538072381905111ae1d7052f1
Fran Rodrigo <franjrodrigop@gmail.com>
2023-03-14 12:32:55 +0100

    deleting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "jquery"
-import "popper"
-import "turbolinks"
 import "bootstrap"
-import "_tree"
